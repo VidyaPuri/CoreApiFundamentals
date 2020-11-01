c50a087cbbf98f3362a51ebc52ef5e156604d083
Vid Cresnik <vcresnik@gmail.com>
2020-11-01 20:06:02 +0100

    Add project files.

---
+++ Models/CampModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreCodeCamp.Models
+{
+    public class CampModel
+    {
+        public string Name { get; set; }
+        public string Moniker { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+        public int Length { get; set; } = 1;
+
+        public string Venue { get; set; }
+        public string LocationVenueNameAddress1 { get; set; }
+        public string LocationVenueNameAddress2 { get; set; }
+        public string LocationVenueNameAddress3 { get; set; }
+        public string LocationVenueNameCityTown { get; set; }
+        public string LocationVenueNameStateProvince { get; set; }
+        public string LocationVenueNamePostalCode { get; set; }
+        public string LocationVenueNameCountry { get; set; }
+
+        public ICollection<TalkModel> Talks { get; set; }
+    }
+}
